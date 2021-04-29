8415a2d8fa2e13f1c40183a479d87f3f0a79ed47
Barna Mink <barnamink@gmail.com>
2021-04-29 10:45:02 -0700

    .

--- hiredis_helper.h
+++ hiredis_helper.h
@@ -23,4 +23,6 @@ int hiredis_rpush(const char *, bstr_t *);
 int hiredis_lpush(const char *, bstr_t *);
 int hiredis_lrange(const char *, int, int, barr_t *);
 
+int hiredis_keys(const char *, barr_t *);
+
 #endif
